23eec7e78cb10cbb1c6ea0ef8cdc6e4f8282e711
Syed Arhan Rizvi <arhanrizvi9@gmail.com>
2025-07-07 15:56:17 +0530

    Fix The page to Page Bug

--- src/app/(home)/blog/[slug]/page.tsx
+++ src/app/(home)/blog/[slug]/page.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'next/navigation';
 import React from 'react';
 
 
-function page() {
+function Page() {
     const params = useParams();
     const {slug} = params;
     alert(slug)
@@ -14,4 +14,4 @@ function page() {
   )
 }
 
-export default page
+export default Page
