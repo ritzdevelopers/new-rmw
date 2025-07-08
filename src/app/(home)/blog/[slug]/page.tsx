7f045125e85a2149a836a29ba45def16c9d37ec4
Abhishek kumar <abhishek8601679@gmail.com>
2025-07-08 15:25:51 +0530

    removed use effect

--- src/app/(home)/blog/[slug]/page.tsx
+++ src/app/(home)/blog/[slug]/page.tsx
@@ -3,26 +3,19 @@
 import React from "react";
 
 function Page() {
-    const params = useParams();
-    const {slug} = params;
-    const getSingleBlog = async ()=>{
-      try {
-        const {data} = await axios.get(`/api/blog/${slug}`);
-         console.log(data);
-         
-      } catch (error) {
-        console.log('====================================');
-        console.log("There are some errors in fetching the single blog ", error);
-        console.log('====================================');
-        alert("Internal Server Error!");
-      }
+  const params = useParams();
+  const { slug } = params;
+  const getSingleBlog = async () => {
+    try {
+      const { data } = await axios.get(`/api/blog/${slug}`);
+      console.log(data);
+    } catch (error) {
+      console.log("====================================");
+      console.log("There are some errors in fetching the single blog ", error);
+      console.log("====================================");
+      alert("Internal Server Error!");
     }
   };
-  useEffect(() => {
-    if (slug) {
-      getSingleBlog();
-    }
-  }, [slug]);
   return <div></div>;
 }
 
