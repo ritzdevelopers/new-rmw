61f3304892235cffd25fac1c0e61ff667981a7b8
Syed Arhan Rizvi <arhanrizvi9@gmail.com>
2025-07-08 16:59:19 +0530

    merge conflict resolve

--- src/app/(home)/blog/[slug]/page.tsx
+++ src/app/(home)/blog/[slug]/page.tsx
@@ -1,8 +1,3 @@
-"use client"
-import axios from 'axios';
-import { useParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
-
 "use client";
 // import { useParams } from "next/navigation";
 import React from "react";
@@ -15,33 +10,19 @@ function Page() {
     //     const {data} = await axios.get(`/api/blog/${slug}`);
     //      console.log(data);
          
-      } catch (error) {
-        console.log('====================================');
-        console.log("There are some errors in fetching the single blog ", error);
-        console.log('====================================');
-        alert("Internal Server Error!");
-      }
-    }
-    useEffect(()=>{
-      if(slug) {
-        getSingleBlog();
-      }
-    }, [slug]);
-
+      // } catch (error) {
+      //   console.log('====================================');
+      //   console.log("There are some errors in fetching the single blog ", error);
+      //   console.log('====================================');
+      //   alert("Internal Server Error!");
+      // }
+    // }
     // id, _id, title, blogTitle, 
   return (
     <div>
       
     </div>
   )
-    //   } catch (error) {
-    //     console.log('====================================');
-    //     console.log("There are some errors in fetching the single blog ", error);
-    //     console.log('====================================');
-    //     alert("Internal Server Error!");
-    //   }
-    // }
-  return <div></div>;
 }
 
 export default Page;
