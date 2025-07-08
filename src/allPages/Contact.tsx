66ba87eb0981c827e702d0b12086ca7bef4701c2
Syed Arhan Rizvi <arhanrizvi9@gmail.com>
2025-07-08 12:54:22 +0530

    Fix More Build Bugs

--- src/allPages/Contact.tsx
+++ src/allPages/Contact.tsx
@@ -1,17 +1,20 @@
-import Form from "./Contactpage/Form"
-import Footer from "@/components/footer/Footer"
-import PagesBanner from "@/components/pagesBanner/PagesBanner"
-
+import Form from "./Contactpage/Form";
+import Footer from "@/components/footer/Footer";
+import PagesBanner from "@/components/pagesBanner/PagesBanner";
 
 const Contact = () => {
   return (
     <>
-    
-    <PagesBanner headingTitle={"Contact Us"}/>
-    <Form/>
-    <Footer/>
+      <PagesBanner
+        headingTitle={"Contact Us"}
+        videoURL={"/videos/bg_pattern.mp4"}
+        mtP={"80px"}
+        mtS={"50px"}
+      />
+      <Form />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default Contact;
