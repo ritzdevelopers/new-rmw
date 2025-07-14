d7a2dd3de281b8ce8e4895761be1a8d207a89d03
Syed Arhan Rizvi <arhanrizvi9@gmail.com>
2025-07-14 16:55:42 +0530

    Blog Posting Successfully Tested

--- src/app/[slug]/DetailPage.tsx
+++ src/app/[slug]/DetailPage.tsx
@@ -160,8 +160,10 @@ const DetailPage: React.FC = () => {
   const [recentB, setRecentB] = useState<RecentBlogs[]>([]);
   // const [searchedBlog, setSearchedBlog] = useState<boolean>(false);
   // NEXT_PUBLIC_SERVER_IMG_PATHs
-  const staticAPI =
-    process.env.NEXT_PUBLIC_SERVER_IMG_PATH || `http://localhost:5000/images`;
+  const staticAPI = process.env.NEXT_PUBLIC_SERVER_IMG_PATH
+  ? `${process.env.NEXT_PUBLIC_SERVER_IMG_PATH}/api/images`
+  : `/api/images`;
+
   useEffect(() => {
     // setSearchedBlog(false);
     const fetchData = async () => {
@@ -320,13 +322,13 @@ const DetailPage: React.FC = () => {
               <img
                 src={
                   isMongo
-                    ? `${staticAPI}/${
-                        singleBlog.blogBanner?.split("/images/")[1]
+                    ? `${staticAPI}${
+                        singleBlog.blogBanner?.split("/images")[1]
                       }`
                     : `/blogs/${singleBlog.blog_image}`
                 }
-                //  ? `${staticAPI}/${
-                //           article.banner.split("/images/")[1]
+                //  ? `${staticAPI}${
+                //           article.banner.split("/images")[1]
                 //         }`
                 //       : `/blogs/${article.banner}`
                 alt={isMongo ? singleBlog.blogTitle : singleBlog.title}
@@ -370,10 +372,10 @@ const DetailPage: React.FC = () => {
                 singleBlog.blogBody?.map((page, idx) => (
                   <div key={idx}>
                     <h2>{page.metaTitle}</h2>
-                    <img
+                    {page.innerImg && <img
                       src={
                         isMongo
-                          ? `${staticAPI}/${page.innerImg.split("/images/")[1]}`
+                          ? `${staticAPI}${page.innerImg.split("/images")[1]}`
                           : `/static/${page.innerImg}`
                       }
                       alt={`Inner ${idx}`}
@@ -573,8 +575,8 @@ const DetailPage: React.FC = () => {
                           <img
                             src={
                               isMongo
-                                ? `${staticAPI}/${
-                                    blog.banner.split("/images/")[1]
+                                ? `${staticAPI}${
+                                    blog.banner.split("/images")[1]
                                   }`
                                 : `/static/${blog.banner}`
                             }
@@ -618,7 +620,7 @@ const DetailPage: React.FC = () => {
                   );
                 })}
             </div>
-            
+
             {/* Related Blogs */}
             <div className={styles.relatedBlogs}>
               {latestRBlogs &&
@@ -635,8 +637,8 @@ const DetailPage: React.FC = () => {
                       <img
                         src={
                           isMongo
-                            ? `${staticAPI}/${
-                                blog.blogBanner?.split("/images/")[1]
+                            ? `${staticAPI}${
+                                blog.blogBanner?.split("/images")[1]
                               }`
                             : `/blogs/${blog.blog_image}`
                         }
