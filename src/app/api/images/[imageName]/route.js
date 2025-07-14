4dd7ee065170183eaaeab13d7e9836570362e6e3
Syed Arhan Rizvi <arhanrizvi9@gmail.com>
2025-07-14 16:55:42 +0530

    Blog Posting Successfully Tested

--- src/app/api/images/[imageName]/route.js
+++ src/app/api/images/[imageName]/route.js
@@ -5,7 +5,7 @@ import path from "path";
 export async function GET(request, context) {
   const { imageName } = await context.params;
 
-  const imagePath = path.join(process.env.SERVER_IMG_PATH, imageName);
+  const imagePath = path.join("D:/ritz-blogs/images", imageName);
 
   try {
     if (!fs.existsSync(imagePath)) {
