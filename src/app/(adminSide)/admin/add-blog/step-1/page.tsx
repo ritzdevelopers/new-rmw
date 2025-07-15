bea64006b0fac457cc740638d2f303302a0a1a31
Syed Arhan Rizvi <arhanrizvi9@gmail.com>
2025-07-15 18:18:56 +0530

    Complete 15-07-2025 tasks

--- src/app/(adminSide)/admin/add-blog/step-1/page.tsx
+++ src/app/(adminSide)/admin/add-blog/step-1/page.tsx
@@ -201,6 +201,7 @@ const Page = () => {
               onChange={(e) => setLocalCategory(e.target.value)}
               className="w-full border rounded-md px-4 py-2"
             >
+               <option value="none-selected">Select Category</option>
               {ritzCategories.length > 0 ? (
                 ritzCategories.map((data, idx) => {
                   return (
@@ -212,7 +213,7 @@ const Page = () => {
               ) : (
                 <p>Categories are loading...</p>
               )}
-              <option value="none-selected">Select Category</option>
+             
             </select>
           </div>
         </div>
