136d64cc19b704f2a040e6dc46ce1e1c7676bddb
SyedArhanRizvi <arhanrizvi9@gmail.com>
2025-10-03 16:47:12 +0530

    Update Meta Title And Description

--- src/app/(home)/rdx-digital-marketing-course/layout.tsx
+++ src/app/(home)/rdx-digital-marketing-course/layout.tsx
@@ -1,15 +1,24 @@
+import type { Metadata } from "next";
 import Footer from "@/components/footer/Footer";
 
-export default async function RDXDigitalMarketing({
+export const metadata: Metadata = {
+  title:
+    "Best Digital Marketing Course in Delhi NCR with placement - RDX | Ritz Media World",
+  description:
+    "RDX | Ritz Media World – Leading Digital Marketing Course in Delhi & across India. Get the most advanced & affordable digital marketing training with assured placement assistance.",
+  keywords:
+    "Digital Marketing Course in Delhi, digital marketing institute in delhi, top ten Digital marketing institutes in delhi, delhi institute of digital marketing, digital marketing training in delhi, Digital Marketing training in Delhi, Digital marketing course with placement in Delhi NCR, Digital marketing training in Delhi NCR, Digital marketing classes in Delhi NCR, Learn digital marketing in Delhi NCR, Digital marketing course in Delhi NCR, Best digital marketing institute in Delhi NCR",
+};
+
+export default function RDXDigitalMarketing({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <>
-    
-      <>{children}</>
-      <Footer></Footer>
+      {children}
+      <Footer />
     </>
   );
 }
