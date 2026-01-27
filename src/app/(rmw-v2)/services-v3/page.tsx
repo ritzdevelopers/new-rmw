89aed67973c1164545743dbe567ef7c740c290c1
SyedArhanRizvi <arhanrizvi9@gmail.com>
2026-01-27 10:34:23 +0530

    resolve service page bug

--- src/app/(rmw-v2)/services-v3/page.tsx
+++ src/app/(rmw-v2)/services-v3/page.tsx
@@ -1,18 +1,18 @@
 import Section1 from '@/components/home-v3/services/Section1'
 import Section2 from '@/components/home-v3/services/Section2'
 import Section3 from '@/components/home-v3/services/Section3';
-import callServicesData from '@/lib/getServicePageData';
+// import callServicesData from '@/lib/getServicePageData';
 
 
 
 export default async function ServicePage1() {
-    const {data} = await callServicesData();
+    // const {data} = await callServicesData();
 
     return (
         <>
             <Section1 />
             <Section2 />
-            <Section3 servicesData={data}></Section3>
+            <Section3 servicesData={[]}></Section3>
         </>
     )
 }
