663135114943e3895036ee5481135015cab8ba71
SyedArhanRizvi <arhanrizvi9@gmail.com>
2026-02-09 12:06:15 +0530

    update ai content

--- src/app/api/update-service-ai-data/route.ts
+++ src/app/api/update-service-ai-data/route.ts
@@ -4,7 +4,7 @@ import { getDBPool } from "@/lib/db";
 export async function GET(req: NextRequest) {
     try {
       const pool = await getDBPool();
-      const allData = await pool.query("SELECT * from service_second");
+      const allData = await pool.query("SELECT * from services");
       return NextResponse.json({ message: "Service AI data updated successfully", data: allData }, { status: 200 });
     } catch (error) {
         console.error("Error updating service AI data:", error);
