f58d67615a53511be9b23c23228ddfd3f2061c8a
SyedArhanRizvi <arhanrizvi9@gmail.com>
2026-03-30 13:20:11 +0530

    create copy brnand compo and use them in digital and celeb

--- src/components/copy/BrandImpactSection2.tsx
+++ src/components/copy/BrandImpactSection2.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { Download } from "lucide-react";
 import Link from "next/link";
-import styles from "./page.module.css"; 
+import styles from "./page.module.css";
 import { BsArrowRight } from "react-icons/bs";
 
 function BrandImpactSection2() {
@@ -175,7 +175,7 @@ function BrandImpactSection2() {
             <div className={`w-full flex ${styles.containerWidth} flex-col lg:flex-row justify-between gap-6`}>
                 {/* Left Side Container  */}
                 <div className="w-full xl:w-full lg:w-[50%] xl:flex-1 min-h-[500px] xl:h-[526px] bg-[#F7F7F7] flex flex-col justify-around px-1 lg:px-8 py-6 sm:py-7 lg:py-8 md:px-6">
-                    <div className="text-center md:text-left mb-2 md:mb-0">
+                    <div className="text-center md:text-left mb-2 md:mb-0 flex flex-col gap-2">
                         <p
                             className="uppercase font-[600] text-[14px] sm:text-[15px] lg:text-[15px] xl:text-[16px] text-[#C99237]"
                             style={{
@@ -184,18 +184,22 @@ function BrandImpactSection2() {
                         >
                             Free Resource
                         </p>
-                        <h2
-                            className="font-[700] text-[21px]  lg:text-[22px] xl:text-[36px]"
-                            style={{
-                                fontFamily: "MontserratBold",
-                            }}
-                        >
-                            2026 Brand Impact Report
-                        </h2>
+
+                        <div>
+                            <p className="font-[700] text-[16px] lg:text-[24px] text-center md:text-left mb-2 md:mb-0">
+                                Download Our
+                            </p>
+
+                            <h2
+                                className="font-[700] text-[21px]  lg:text-[22px] xl:text-[36px]"
+                                style={{
+                                    fontFamily: "MontserratBold",
+                                }}
+                            >
+                                2026 Brand Impact Report
+                            </h2>
+                        </div>
                     </div>
-                    <p className="font-[700] text-[16px] lg:text-[24px] text-center md:text-left mb-2 md:mb-0">
-                        Download Our
-                    </p>
 
                     <p
                         className="font-[400] text-[13px]  lg:text-[15px] xl:text-[16px] text-center md:text-left mb-2 md:mb-0"
@@ -203,11 +207,11 @@ function BrandImpactSection2() {
                             fontFamily: "PoppinsRegular",
                         }}
                     >
-                        Get exclusive insights into real estate and lifestyle brand marketing trends, strategies, and ROI benchmarks for 2025.
+                        Get exclusive insights into real estate and lifestyle brand <br className="hidden lg:block" /> marketing trends, strategies, and ROI benchmarks for 2025.
                     </p>
 
                     <ul
-                        className="font-[400] text-[13px] md:text-[14px] lg:text-[15px] xl:text-[16px] list-none md:list-disc pl-4 flex flex-col gap-2 sm:gap-3 text-center md:text-left mb-4 md:mb-0"
+                        className="font-[400] text-[13px] md:text-[14px] lg:text-[15px] xl:text-[16px] list-none md:list-disc pl-4 flex  flex-col marker:text-2xl gap-2 sm:gap-3 text-center md:text-left mb-4 md:mb-0"
                         style={{
                             fontFamily: "PoppinsRegular",
                         }}
@@ -280,12 +284,12 @@ function BrandImpactSection2() {
                             fontFamily: "PoppinsRegular",
                         }}
                     >
-                        Let our experts analyze your current brand positioning and provide
+                        Let our experts analyze your current brand positioning and <br className="hidden lg:block" /> provide
                         actionable recommendations.
                     </p>
 
                     <ul
-                        className="list-none md:list-disc pl-4 flex flex-col gap-2 sm:gap-3 font-[400] text-[13px] md:text-[15px] lg:text-[15px] xl:text-[16px] text-center md:text-left"
+                        className="list-none md:list-disc pl-4 flex flex-col gap-2 sm:gap-3 font-[400] text-[13px] md:text-[15px] lg:text-[15px] marker:text-2xl xl:text-[16px] text-center md:text-left"
                         style={{
                             fontFamily: "PoppinsRegular",
                         }}
@@ -317,37 +321,44 @@ function BrandImpactSection2() {
             </div>
 
             {/* Row 3  */}
-            <div className={`w-full flex ${styles.containerWidth} justify-center items-center`}>
-                {/* Center Align Container  */}
-                <div className="flex flex-col gap-2 sm:gap-3 justify-center text-center items-center ">
-                    <h2
-                        className="font-[800] text-[19px] md:text-[28px] lg:text-[36px]"
-                        style={{
-                            fontFamily: "MontserratExtraBold",
-                        }}
-                    >
-                        Ready to Elevate Your Brand?
-                    </h2>
-                    <p
-                        className="font-[400] text-[16px] md:text-[24px] lg:text-[30px]"
-                        style={{
-                            fontFamily: "OpenSansRegular",
-                        }}
-                    >
-                        Let's discuss your next brand-elevating campaign
-                    </p>
-                    <button
-                        onClick={() => window.open("https://ritzmediaworld.com/contact.html", "_blank")}
-                        className="mt-4 cursor-pointer text-white  flex justify-between gap-2 border-b border-black pb-2"
-                        style={{
-                            fontFamily: "OpenSansBold",
-                        }}
-                    >
-                        <p className="text-black font-[600] text-[16px]">Schedule Free Consultation</p>
-                        <BsArrowRight className="w-[26px] h-[26px] text-black" />
-                    </button>
+                 {/* Row 3  */}
+                     <div className={`w-full flex ${styles.containerWidth} justify-center items-center`}>
+                    {/* Center Align Container  */}
+                    <div className="flex flex-col gap-2 sm:gap-3 justify-center text-center items-center bg-[#F5F5F5] min-h-[200px] sm:min-h-[240px] lg:min-h-[279px] w-full px-4 sm:px-6 lg:px-0 py-8 sm:py-10 lg:py-0">
+                        <h2
+                            className="font-[800] text-[19px] md:text-[28px] lg:text-[36px]"
+                            style={{
+                                fontFamily: "MontserratExtraBold",
+                            }}
+                        >
+                            Ready to Elevate Your Brand?
+                        </h2>
+                        <p
+                            className="font-[400] text-[16px] md:text-[24px] lg:text-[30px]"
+                            style={{
+                                fontFamily: "OpenSansRegular",
+                            }}
+                        >
+                            Let's discuss your next brand-elevating campaign
+                        </p>
+                        <button
+                            onClick={() =>
+                                window.open(
+                                    "https://ritzmediaworld.com/contact.html",
+                                    "_blank",
+                                )
+                            }
+                            className="w-full sm:w-[260px] lg:w-[282px] h-[48px] sm:h-[50px] lg:h-[54px] mt-4 bg-[#C99237] cursor-pointer text-white font-[700] text-[14px] sm:text-[14.5px] lg:text-[15px] rounded-[5px] hover:bg-[#B8822F] transition-colors  s1-btn-gold"
+                            style={{
+                                fontFamily: "OpenSansBold",
+                            }}
+                        >
+                            <p className="text-white">
+                                Schedule Free Consultation
+                            </p>
+                        </button>
+                    </div>
                 </div>
-            </div>
             {/* Modal */}
             {modal.open && (
                 <div className="fixed inset-0 flex items-center justify-center z-[9999] bg-black/50 backdrop-blur-sm">
