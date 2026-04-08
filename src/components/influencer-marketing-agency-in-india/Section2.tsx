2d2e905636255b23eb5531eb2cd3f4fd0c2147a6
SyedArhanRizvi <arhanrizvi9@gmail.com>
2026-04-08 15:33:49 +0530

    update responsive design

--- src/components/influencer-marketing-agency-in-india/Section2.tsx
+++ src/components/influencer-marketing-agency-in-india/Section2.tsx
@@ -9,7 +9,7 @@ export default function Section2() {
     <section className="w-full flex items-center justify-center py-[35px] lg:py-[70px] bg-white">
       <div className="w-[100%] md:w-[100%] lg:w-[100%] xl:w-[74%] text-center mx-auto px-4 md:px-[40px] lg:px-[50px] min-[1370px]:!w-[1300px] min-[1370px]:!max-w-[1300px] min-[1370px]:mx-auto">
         <p
-          className="text-black font-[700] text-[20px] sm:text-[20px] md:text-[20px] lg:text-[24px] leading-[30px]   md:leading-[30px] lg:leading-[30px] xl:leading-[40px] xl:max-w-[1000px]   mx-auto"
+          className="text-black font-[700] text-[20px] sm:text-[20px] md:text-[20px] lg:text-[24px] leading-[30px]   md:leading-[30px] lg:leading-[40px] xl:leading-[40px] xl:max-w-[1000px]   mx-auto"
           style={{ fontFamily: "MontserratRegular" }}
         >
           Influencer marketing goes beyond hype, it’s all about trust, reach, and results.
