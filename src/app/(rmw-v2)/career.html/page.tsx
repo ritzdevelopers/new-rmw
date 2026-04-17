167cc9d60d587d4cd6c143343930b0d7186c0350
varunmathur19 <varunmathur817@gmail.com>
2026-04-17 17:53:26 +0530

    career page

---
+++ src/app/(rmw-v2)/career.html/page.tsx
@@ -0,0 +1,12 @@
+// import Banner from "@/components/content-marketing/Banner
+
+import Banner from "@/components/carrer/banner"
+
+function Career(){
+    return(
+        <>
+            <h1><Banner/></h1>
+        </>
+    )
+}
+export default Career
---
+++ src/components/carrer/banner.tsx
@@ -0,0 +1,8 @@
+function Banner(){
+    return(
+        <>
+        <h1>hello</h1>
+        </>
+    )
+}
+export default Banner
