a4f2038ad1180854701aa5a5ad7c62dde29d176a
itzz-aryan-121 <arhanrizvi9@gmail.com>
2026-05-25 19:17:00 +0530

    update latest code

--- src/app/(home)/work.html/page.tsx
+++ src/app/(home)/work.html/page.tsx
@@ -55,6 +55,7 @@ const Page = () => {
       <link rel="preload" href="/videos/bg_pattern.mp4" as="video" type="video/mp4" />
       <link rel="preload" href="/work-images/work-banner.jpg" as="image" />
       
+      
       <section className="flex flex-col items-center w-full">
         <Suspense fallback={
           <div 
