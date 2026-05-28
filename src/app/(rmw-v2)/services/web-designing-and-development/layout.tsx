31f120e63c3b3696d73d2dcfeb622cbe93a417e1
itzz-aryan-121 <aryan.22b0131042@abes.ac.in>
2026-05-28 17:10:05 +0530

    new changes

---
+++ src/app/(rmw-v2)/services/web-designing-and-development/layout.tsx
@@ -0,0 +1,9 @@
+export { metadata } from "@/app/(rmw-v2)/web-development2/layout";
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
